58ec77789badcd94b70b775db40dcbd291db71d0
sreekanthpullat <sreekanthpullat@gmail.com>
2023-09-28 11:24:08 +0530

    added lint staged

--- packages/web/src/App.test.tsx
+++ packages/web/src/App.test.tsx
@@ -5,6 +5,5 @@ import App from "./App";
 it("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  screen.debug();
   expect(linkElement).toBeInTheDocument();
 });
